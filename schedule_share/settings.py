45b75d1e7d1b8586424cfdce1705f4c5a0fc6828
Achintya Rajan <rajan.achintya03@gmail.com>
2021-12-05 17:17:12 -0600

    Update settings.py

--- schedule_share/settings.py
+++ schedule_share/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
 # ]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CSRF_COOKIE_SECURE = True
 
